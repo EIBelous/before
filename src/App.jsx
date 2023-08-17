35563353af46c0c6091c64b447e3d63068aef66d
EIBelous <evgen.i.bilous@gmail.com>
2023-08-17 14:18:57 +0300

    before display cart elements

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,12 @@ export default function App() {
   function addToCart(id, sku){
     setCart ((items)=>{
       const itemInCart = items.find((i)=> i.sku === sku)
-      itemInCart.quantity++
+      if (itemInCart) {
+        return items.map((i)=>
+        i.sku === sku ? {...i, quantity: i.quantity + 1 } : i );
+      } else{
+        return [...items, {id,sku, quantity:1}]
+      }
 
     })
   }
@@ -27,7 +32,8 @@ export default function App() {
           <Routes>
           <Route path="/" element={<h1>Welcome to cart Rock fitness</h1>}/>
           <Route path="/:category" element={<Products />}/>
-          <Route path="/:category/:id" element={<Detail />}/>
+          <Route path="/:category/:id" 
+          element={<Detail addToCart={addToCart} />}/>
           <Route path="/cart" element={<Cart />}/>
           </Routes>
             
