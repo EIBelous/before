f1a1905b0047c8dcc6d93d3a36607c685b945350
EIBelous <evgen.i.bilous@gmail.com>
2023-08-25 09:42:56 +0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,11 @@ export default function App() {
   }
 
 
-
+function updateQuantity(sku, quantity) {
+  setCart((items)=>{
+    return items.map((i)=> i.sku === sku ? {...i, quantity} : i)
+  })
+}
   return (
     <>
       <div className="content">
@@ -34,7 +38,7 @@ export default function App() {
           <Route path="/:category" element={<Products />}/>
           <Route path="/:category/:id" 
           element={<Detail addToCart={addToCart} />}/>
-          <Route path="/cart" element={<Cart />}/>
+          <Route path="/cart" element={<Cart cart={cart} updateQuantity={updateQuantity}/>}/>
           </Routes>
             
         </main>
