332fe4512949623ab7dbb0eb48c08c3096c86131
EIBelous <evgen.i.bilous@gmail.com>
2023-09-05 23:43:50 +0300

    create checkout

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import useFetchAll from "./services/useFetchAll"
 import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
 
 export default function Cart({ cart, updateQuantity }) {
+  const navigate=useNavigate()
   const urls = cart.map((i) => `products/${i.id}`);
   const { data: products, loading, error } = useFetchAll(urls);
 
@@ -49,6 +51,11 @@ export default function Cart({ cart, updateQuantity }) {
       <h1>{numItemsInCart === 0 ? "Your cart is empty" : 
       `${numItemsInCart} items in my cart`}</h1>
       <ul>{cart.map(renderItem)}</ul>
+     {cart.length > 0 && (
+      <button className="btn btn-primary" onClick={()=>navigate("/checkout")}>
+        Checkout
+      </button>)
+      }
     </section>
   );
 }
