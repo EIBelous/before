b7af519a8bbebb3de86bde1026495ea4ed0def65
EIBelous <evgen.i.bilous@gmail.com>
2023-10-24 09:05:59 +0300

    e

--- src/checkout.jsx
+++ src/checkout.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { saveShippingAddress } from "./services/shippingService";
 
 const STATUS = {
-  IDLE : "IDLE",
-  SUBMITTED : "SUBMITTED",
+  IDLE: "IDLE",
+  SUBMITTED: "SUBMITTED",
   SUBMITTING: "SUBMITTING",
   COMPLETED: "COMPLETED",
 };
@@ -16,73 +17,72 @@ const emptyAddress = {
 
 export default function Checkout({ cart, emptyCart }) {
   const [address, setAddress] = useState(emptyAddress);
-const [status, setStatus] = useState(STATUS.IDLE)
-const [saveError, setSaveError] = useState(null)
+  const [status, setStatus] = useState(STATUS.IDLE);
+  const [saveError, setSaveError] = useState(null);
+  const [touched, setTouched] = useState({});
 
-// Derived state
-const errors = getErrors(address) 
-const isValid = Object.keys(errors).length === 0;
+  // Derived state
+  const errors = getErrors(address);
+  const isValid = Object.keys(errors).length === 0;
 
   function handleChange(e) {
-    e.persist()
-    setAddress((curAdress)=>{
-        return{
-            ...curAdress,
-            [e.target.id]:e.target.value
-        }
-
-    })
+    e.persist(); // persist the event
+    setAddress((curAddress) => {
+      return {
+        ...curAddress,
+        [e.target.id]: e.target.value,
+      };
+    });
   }
-//ddddddd
 
   function handleBlur(event) {
-    // TODO
+    event.persist();
+    setTouched((cur) => {
+      return { ...cur, [event.target.id]: true };
+    });
   }
-
 
   async function handleSubmit(event) {
-    event.preventDefault()
-   setStatus(STATUS.SUBMITTING)
-   if (isValid) {
-       try{
-    await saveShippingAddress(address)
-    emptyCart()
-    setStatus(STATUS.COMPLETED)
-   }
-   catch(e){
-    setSaveError(e)
-   }
-  } else{
-    setStatus(STATUS.SUBMITTED)
-  }
-}
-   
-  function getErrors(address){
-    const result = {}
-    if (!address.city) result.city = "City is required"
-    if (!address.country) result.country = "Country is required"
-    return result
+    event.preventDefault();
+    setStatus(STATUS.SUBMITTING);
+    if (isValid) {
+      try {
+        await saveShippingAddress(address);
+        emptyCart();
+        setStatus(STATUS.COMPLETED);
+      } catch (e) {
+        setSaveError(e);
+      }
+    } else {
+      setStatus(STATUS.SUBMITTED);
+    }
   }
 
-  if (saveError) throw saveError
-  if (status === STATUS.COMPLETED) {
-    return(
-      <h1 className="">Thanks fot order</h1>
-    )
+  function getErrors(address) {
+    const result = {};
+    if (!address.city) result.city = "City is required";
+    if (!address.country) result.country = "Country is required";
+    return result;
   }
+
+  if (saveError) throw saveError;
+  if (status === STATUS.COMPLETED) {
+    return <h1>Thanks for shopping!</h1>;
+  }
+
   return (
     <>
       <h1>Shipping Info</h1>
       {!isValid && status === STATUS.SUBMITTED && (
-        <div role="alert"> 
-        <p>Please fix folowing errors:</p>
-        <ul>
-          {Object.keys(errors).map((key)=>{
-            return <li key={key}>{errors[key]}</li>
-          } )}
-        </ul>
+        <div role="alert">
+          <p>Please fix the following errors:</p>
+          <ul>
+            {Object.keys(errors).map((key) => {
+              return <li key={key}>{errors[key]}</li>;
+            })}
+          </ul>
         </div>
-      )} 
+      )}
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="city">City</label>
@@ -94,6 +94,9 @@ const isValid = Object.keys(errors).length === 0;
             onBlur={handleBlur}
             onChange={handleChange}
           />
+          <p role="alert">
+            {(touched.city || status === STATUS.SUBMITTED) && errors.city}
+          </p>
         </div>
 
         <div>
@@ -111,6 +114,10 @@ const isValid = Object.keys(errors).length === 0;
             <option value="United Kingdom">United Kingdom</option>
             <option value="USA">USA</option>
           </select>
+
+          <p role="alert">
+            {(touched.country || status === STATUS.SUBMITTED) && errors.country}
+          </p>
         </div>
 
         <div>
@@ -118,7 +125,7 @@ const isValid = Object.keys(errors).length === 0;
             type="submit"
             className="btn btn-primary"
             value="Save Shipping Info"
-            disabled = {status === STATUS.SUBMITTING}
+            disabled={status === STATUS.SUBMITTING}
           />
         </div>
       </form>
